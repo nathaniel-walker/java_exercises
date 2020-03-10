49e2734358617351d9aa86d09bc8f1599fc6990c
nathaniel-walker <61957052+nathaniel-walker@users.noreply.github.com>
2020-03-10 11:48:03 -0400

    Add files via upload
    Generic HelloWorld Java program.

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+* @author - Nathaniel Walker
+* @version - 1.0
+* date: 3/9/2020
+*/
+
+// Hello World class
+public class HelloWorld {
+	// main method
+	public static void main(String[] args) {
+		// print statement
+		System.out.println("Hello World!");
+	}
+}
